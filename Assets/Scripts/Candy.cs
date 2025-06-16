e66ae4272bb1e7fbd8857ce53977d46915cecd79
Lucas Araujo <lsa77793@gmail.com>
2025-06-15 22:58:54 -0300

    Actualiza propiedades de SpriteRenderer y Transform

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -1,7 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Candy : MonoBehaviour
 {
+
+    private static Color seletedColor = new Color(0.5f, 0.5f, 0.5f, 1f);
+    private static Candy previousSelected = null;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+
+    public int id;
+
+    private Vector2[] adjacenDiections = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    };
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
