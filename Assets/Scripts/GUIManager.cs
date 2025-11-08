436afa857ab1ebbaa9ffe14a915c554b6076c58a
Lucas Araujo <lsa77793@gmail.com>
2025-11-07 23:54:43 -0300

    Agregar visualización de contador de movimientos

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIManager: MonoBehaviour
 {
+    public Text movesText;
+    private int moveCounter = 30;
 
-    
+    void Start()
+    {
+        moveCounter = 30;
+        movesText.text = "Moves: " + moveCounter;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
 }
