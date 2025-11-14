996b259c04c0a12a7e5206cf349999aa8a306a3a
Lucas Araujo <lsa77793@gmail.com>
2025-11-14 15:34:20 -0300

    Agregar visualización de puntuación y propiedad a GUIManager

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -4,13 +4,35 @@ using UnityEngine.UI;
 
 public class GUIManager: MonoBehaviour
 {
-    public Text movesText;
+    public Text movesText, scoreText;
     private int movesCounter;
+    private int score;
+
+
+    public int MovesCounter
+    {
+        get { return movesCounter; }
+        set
+        {
+            movesCounter = value;
+            movesText.text = "Moves: " + movesCounter;
+        }
+    }
+    public int Score
+    {
+        get { return score; }
+        set 
+        { 
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
     void Start()
     {
-        
+        score = 0;
         movesCounter = 30;
         movesText.text = "Moves: " + movesCounter;
+        scoreText.text = "Score: " + score;
     }
 
     private void Update()
